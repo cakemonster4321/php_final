1e2a33e60c87627a5eded11e63c2008dfaced352
Peter_Lin <a31438199@gmail.com>
2025-05-31 03:35:09 +0800

    sql connection

--- db_config.php
+++ db_config.php
@@ -1,18 +1,19 @@
 <?php
-$host = getenv('MYSQLHOST');
-$db   = getenv('MYSQLDATABASE');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
+$host = 'mysql.railway.internal';
+$port = 3306;
+$db   = 'railway';
+$user = 'root';
+$pass = 'NmtbdQmnCUwuxVwMniMrjoArUlqMFoYe';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
+
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (PDOException $e) {
     exit('Database connection failed: ' . $e->getMessage());
 }
-?>
